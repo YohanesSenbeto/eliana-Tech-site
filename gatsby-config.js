10f1c1953afeebecdaffb0a4222880396a388b50
Yohanes Senbeto <121575345+YohanesSenbeto@users.noreply.github.com>
2023-08-13 21:25:01 +0000

    changed

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,13 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Sydney Ecommerce Theme`,
+    title: `eliana tech site`,
     siteUrl: `https://jamm.matter.design`,
   },
   plugins: [
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Sydney Ecommerce Theme`,
+        name: `eliana tech site`,
         short_name: `Sydney`,
         start_url: `/`,
         background_color: `#000000`,
